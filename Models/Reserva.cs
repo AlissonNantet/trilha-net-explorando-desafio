f4b239da471e7b1c13c05aa825ffac8903f914d8
Alisson Nantet <alissonnantetr@gmail.com>
2024-10-09 02:01:30 -0300

    DESAFIO FINALIZADO

---
+++ Models/Reserva.cs
@@ -0,0 +1,62 @@
+namespace Desafio_2_Hotel.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            // *IMPLEMENTE AQUI* --- FEITO!
+            if (hospedes.Count <= Suite.Capacidade)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                // *IMPLEMENTE AQUI* --- FEITO!
+                throw new Exception("Quandidade de hóspedes ultrapassou o limite do quarto");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
+            // *IMPLEMENTE AQUI* --- FEITO!
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // TODO: Retorna o valor da diária
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            // *IMPLEMENTE AQUI* --- FEITO!
+            decimal valor = (DiasReservados * Suite.ValorDiaria);
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            // *IMPLEMENTE AQUI* --- FEITO!
+            if (DiasReservados >= 10)
+            {
+                decimal decimo = valor / 10;
+                valor =  valor - decimo;
+            }
+
+            return valor;
+        }
+    }
+}
